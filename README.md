637118356fe1916fb6cadcaee2079337cc06eb7e
Simon Westphahl <simon@westphahl.net>
2023-01-30 14:33:46 +0100

    Implement PoC of Git credential helper
    The git-credential helper is implemented using async and the Tokio
    runtime as that is what the Octrocrab crate uses.
    
    The command is configured using the following environment variables:
    * GITHUB_APP_ID: the ID of the Github app used for authentication
    * GITHUB_APP_PRIVATE_KEY: the path to the private key for the app
    * GITHUB_URL (optional): the URL of the Github API (default:
      https://api.github.com)
    
    Tokens are currently not cached and a repository clone will lead to
    multiple calls of the credential helper which will alway request and
    return a new installation auth token.
    
    This means that we could theoretically run into rate limits.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Git credential helper: Github App auth
+# git-credential-github-app-auth
+
+Git [credential
+helper](https://git-scm.com/docs/gitcredentials#_custom_helpers) using GitHub
+[App
+authentication](https://docs.github.com/en/developers/apps/building-github-apps/authenticating-with-github-apps).
+
+
+Configure the Git credential helper in `~/.gitconfig`:
+```git
+[credential "https://github.com"]
+    helper = "github-app-auth"
+    useHttpPath = true
+```
+
+Make sure the helper binary `git-credential-github-app-auth` is in your path
+and the following environment variables are set:
+
+```sh
+export GITHUB_APP_ID=12345
+export GITHUB_APP_PRIVATE_KEY=$(< /path/to/private/key.pem)
+```
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,75 @@
+use futures_util::stream::StreamExt;
 use octocrab::Octocrab;
 use secrecy::ExposeSecret;
+use std::error::Error;
+use std::fs;
+use std::result::Result;
+use tokio::io;
+use tokio_util::codec::{FramedRead, LinesCodec};
 
 #[tokio::main]
-async fn main() -> octocrab::Result<()> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let app_id = read_env_var("GITHUB_APP_ID").parse::<u64>().unwrap().into();
-    let app_private_key = read_env_var("GITHUB_APP_PRIVATE_KEY");
+    let app_private_key = fs::read_to_string(read_env_var("GITHUB_APP_PRIVATE_KEY"))?;
+    // TODO: check permissions of private key
+    let github_url =
+        std::env::var("GITHUB_URL").unwrap_or_else(|_| "https://api.github.com".to_string());
     let key = jsonwebtoken::EncodingKey::from_rsa_pem(app_private_key.as_bytes()).unwrap();
 
-    // TODO: make this env vars or cli params
-    let organization = "westphahl";
-    let repository = "git-credential-helper-github-app-auth";
+    let mut repo_path = String::new();
+    let mut protocol = String::new();
+    let mut reader = FramedRead::new(io::stdin(), LinesCodec::new());
+    loop {
+        let line = match reader.next().await {
+            Some(l) => l?,
+            None => break,
+        };
+        // eprintln!("Got line {:?}", line);
+        if line.is_empty() {
+            break;
+        }
+        let (attr, value) = line
+            .split_once('=')
+            .ok_or("Invalid input format (must be 'attr=value')")?;
+        match attr {
+            "path" => repo_path.push_str(value),
+            "protocol" => protocol.push_str(value),
+            _ => {}
+        }
+        // eprintln!("Got attribute {:?} => {:?}", attr, value);
+    }
 
-    let octocrab = Octocrab::builder().app(app_id, key).build()?;
+    if protocol != "https" {
+        Err("Cannot handle non-https protocols")?;
+    }
 
+    if repo_path.is_empty() {
+        Err("No repo path provided")?;
+    }
+
+    let (organization, repo_path) = repo_path
+        .split_once('/')
+        .ok_or("Failed to extract organization and repo name (expected format 'org/repo')")?;
+    // Remove .git suffix that can be part of the clone URL
+    let repository = repo_path.strip_suffix(".git").unwrap_or(repo_path);
+
+    let octocrab = Octocrab::builder()
+        .base_url(github_url)?
+        .app(app_id, key)
+        .build()?;
     let installation = octocrab
         .apps()
         .get_repository_installation(organization, repository)
         .await?;
 
-    println!("Installation: {:?}", installation);
+    // eprintln!("Installation: {:?}", installation);
 
     let (_, secret_token) = octocrab.installation_and_token(installation.id).await?;
     let token = secret_token.expose_secret();
 
-    println!("Token {}", token);
+    // eprintln!("Token {}", token);
+    println!("username=x-access-token");
+    println!("password={}", token);
 
     Ok(())
 }
